de34281ee400821b73d9d76ab3bec0352d1d0342
hasibferdous <hasibferdous.ict@gmail.com>
2022-10-06 23:39:57 +0600

    features

--- src/Component/Pricing/Pricing.js
+++ src/Component/Pricing/Pricing.js
@@ -1,15 +1,39 @@
 import React from 'react';
+import PricingOption from '../PricingOption/PricingOption';
 
 const Pricing = () => {
     const pricingOption =[
-        {id:1, name: 'free', price:0},
-        {id:2, name: 'medium', price:50},
-        {id:3, name: 'premium', price:500},
+        {id:1, name: 'free', price:0, features: [
+            'Awesome Feature',
+            'New Feature',
+            'Exclusive Feature'
+        ] },
+
+        {id:2, name: 'medium', price:50, features: [
+            'Awesome Feature',
+            'New Feature',
+            'Exclusive Feature'
+        ] },
+
+        {id:3, name: 'premium', price:500, features: [
+            'Awesome Feature',
+            'New Feature',
+            'Exclusive Feature'
+        ] },
     ]
     return (
         <div>
             <h2 className='text-5xl bg-indigo-300 p-12 font-bold text-white'>This is the best deal in the town</h2>
-        
+        <div className='grid md:grid-cols-3 gap-3 mt-4'>
+        {
+            pricingOption.map(option => <PricingOption
+                key={option.id}
+                option = {option}
+            >
+
+            </PricingOption>)
+        }
+        </div>
         </div>
     );
 };
--- src/Component/PricingOption/PricingOption.js
+++ src/Component/PricingOption/PricingOption.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Feature from '../Feature/Feature';
 
-const PricingOption = () => {
+const PricingOption = ({option}) => {
+    const {features} = option;
     return (
-        <div>
-            
+        <div className='bg-indigo-300 m-3 p-6 rounded-md'>
+            <div>
+                <h3><span className='text-6xl font-bold text-white'>{option.price}</span>
+                <span   className='text-2xl text-gray-50'>/mon</span></h3>
+                <p className='text-3xl my-4'>{option.name}</p>
+            </div>
+            {
+                features.map((feature, idx)=>  <Feature 
+                key={idx} 
+                feature={feature}
+                ></Feature>)
+            }
+            <button className='bg-green-500 md:rounded text-white w-full m-3 p-3 font-bold'>Buy Now</button>
         </div>
     );
 };
